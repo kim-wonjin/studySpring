8a2b482cdbf35c81e7db6d404add4ef9f95f163e
김원진 <sobaek2000@naver.com>
2022-02-19 16:08:02 +0900

    프로젝트 이해

---
+++ studySpring/src/main/java/study/studySpring/controller/helloController.java
@@ -0,0 +1,16 @@
+package study.studySpring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class helloController {
+
+    @GetMapping("hello")  // url /hello 들어오면 밑에를 호출
+    public String hello (Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";  // resources/templates/hello.html 로 연결
+    }
+}
+
---
+++ studySpring/src/test/java/study/studySpring/StudySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package study.studySpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
