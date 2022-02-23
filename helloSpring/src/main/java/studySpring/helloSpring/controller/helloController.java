2a7d10373a21a69f3c3e82aff5ae0061ce42d10e
김원진 <sobaek2000@naver.com>
2022-02-23 18:21:26 +0900

    mvc와 템플릿엔

--- helloSpring/src/main/java/studySpring/helloSpring/controller/helloController.java
+++ helloSpring/src/main/java/studySpring/helloSpring/controller/helloController.java
@@ -3,6 +3,7 @@ package studySpring.helloSpring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class helloController {
@@ -11,5 +12,10 @@ public class helloController {
     public String hello (Model model) {
         model.addAttribute("data", "hello!~~~!");
         return "hello";  // resources/templates/hello.html 로 연결
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc (@RequestParam("name")String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
