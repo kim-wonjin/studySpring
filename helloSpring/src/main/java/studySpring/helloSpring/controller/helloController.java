e95f9ce670e512a3afd47170e08df15eb11ee9c7
김원진 <sobaek2000@naver.com>
2022-02-26 15:34:40 +0900

    api 방식

--- helloSpring/src/main/java/studySpring/helloSpring/controller/helloController.java
+++ helloSpring/src/main/java/studySpring/helloSpring/controller/helloController.java
@@ -4,18 +4,47 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class helloController {
 
+    // 정적 컨텐츠
     @GetMapping("hello")  // url /hello 들어오면 밑에를 호출
     public String hello (Model model) {
         model.addAttribute("data", "hello!~~~!");
         return "hello";  // resources/templates/hello.html 로 연결
     }
+    // MVC와 템플릿 엔진
     @GetMapping("hello-mvc")
     public String helloMvc (@RequestParam("name")String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+    //API
+    @GetMapping("hello-String")
+    @ResponseBody
+    public String helloString (@RequestParam("name")String name) {
+        return "hello " + name;
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi (@RequestParam("name")String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+
+        // cmd N : generate 단축키
+        // Getter Setter
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
